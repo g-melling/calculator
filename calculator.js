c17af4a07308863d84a264f92ad4004b7b5fb3b0
Georgina Melling <156523659+georginamelling@users.noreply.github.com>
2025-12-01 12:44:14 +0000

    calculator JS functions updated

--- calculator.js
+++ calculator.js
@@ -5,22 +5,57 @@ let currentInput = '';
 let previousInput = '';
 let operator = '';
 
+buttons.forEach(button => {
+    button.addEventListener('click', handleButtonClick);
+});
 
-function calculate(a, b, operator) {
-    if (operator === '+') {
-        return a + b;
+function handleButtonClick(event) {
+    const buttonText = event.target.innerText;
+
+    if (buttonText >= 0 && buttonText <= 0 || buttonText === ".") {
+        // update display
+        currentInput += buttonText;
+        updateDisplay(currentInput);
     }
-    else if (operator === '-') {
-        return a - b;
+
+    else if (['+', '-', '/', 'X', '%'].includes(buttonText)) {
+        if (operator && currentInput) {
+            calculate();
+        }
+        operator = buttonText;
+        previousInput = currentInput;
+        currentInput = '';
     }
-    else if (operator === '*') {
-        return a * b;
+
+    else if (buttonText === '=') {
+        if currentInput !== '' {
+            calculate();
+        }
     }
-    else if (operator === '/') {
-        return a / b;
+
+    else if (buttonText === '+/-') {
+        if (currentInput) {
+            currentInput = `${-}currentInput`;
+        }
     }
+
+// handle +- function
+
+// handle percent function
+
+// handle AC
+
+// handle clear
 }
 
+
+
+
+
+
+
+// calculate function (check if current and previous input have values)
+
 function updateDisplay(value) {
-    display.innerText = value;
+    display.innerText = value || "0";
 }
